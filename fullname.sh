47d94a5aecd24cf69e3502498cc83322239ecc05
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2012-09-08 11:06:56 +0900

    add example shell scirpts

---
+++ fullname.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# for Bash 4.0
+
+declare -A fullnames
+fullnames[ichiro]='Ichiro Suzuki'
+fullnames[tomohiro]='Tomohiro TAIRA'
+
+
+
+
+function get_name()
+{
+  local fullname=${fullnames[$1]}
+  if [ -n "$fullname" ]; then
+    echo $fullname
+  else
+    echo "Not found"
+  fi
+}
+
+get_name $1
